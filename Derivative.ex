62c20fe80c2c897e7f393373a257df32915455a4
JonasDaderman <Daderman.Jonas@gmail.com>
2023-01-19 12:55:03 +0100

    Added mul, add, div, ln, powr

--- Derivative.ex
+++ Derivative.ex
@@ -5,16 +5,29 @@ defmodule Derivative do
 #f(x) = C, d(f(x))/dx = 0
 
 @type literal() :: {:num, number()} | {:var, atom()}
-@type expr() :: literal() | {:add, expr(), expr()} | {:mul, expr(), expr()} | {:ln, :var} | {:div, expr(), expr()} | {:powr, expr(), expr()}
+@type expr() :: literal() |
+{:add, expr(), expr()} |
+{:mul, expr(), expr()} |
+{:ln, expr()} |
+{:div, expr(), expr()} |
+{:powr, expr(), expr()} |
+{:div, expr(), expr()}
 
 def test() do
-  #expression = {:add, {:mul, {:num, -2}, {:var, :x}}, {:num, 4}}
-  #expression2 = {:ln, :x}
+  #expression = {:add, {:mul, {:num, -2}, {:var, :x}}, {:mul, {:var, :x}, {:num, 5}}}
+  #expression2 = {:add, {:mul, {:num, 2}, {:var, :x}}, {:num, 4}}
   #deriv(expression2, :x)
-  expression3 = {:powr, :x, 3}
-  deriv(expression3, :x)
+  expression3 = {:add, {:powr, {:var, :x}, {:num, 3}}, {:num, 4}}
+  expressionLN = {:ln, {:mul, {:var, :x}, {:num, 2}}}
+  d = deriv(expression3, :x)
+  ln = deriv(expressionLN, :x)
   #deriv(expression, :x)
-#ADDED THIS COMMENT
+  IO.write("expression: #{pprint(expression3)}\n")
+  IO.write("Derivative: #{pprint(d)}\n")
+  IO.write("Simplified: #{pprint(simplify(d))}\n")
+  IO.write("expression: #{pprint(expressionLN)}\n")
+  IO.write("Derivative: #{pprint(ln)}\n")
+  IO.write("simplified: #{pprint(simplify(ln))}\n" )
 end
 
 def deriv({:num, _}, _) do {:num, 0} end
@@ -29,13 +42,44 @@ def deriv({:mul, expr1, expr2}, v) do {:add, {:mul, deriv(expr1, v), expr2}, {:m
 #f(x) = ln(x), d(f(x))/dx = 1/x
 def deriv({:ln, x}, x) do {:div, {:num, 1}, {:var, x}} end
 #f(x) = x^n, d(f(x))/dx = nx^(n-1)
-def deriv({:powr, expr1, expr2}, x) do {:mul, expr2, {:pow, expr1, {:add, expr2, {:num, -1}}}} end
+def deriv({:powr, expr1, {:num, n}}, x) do {:mul, {:mul, {:num, n}, {:powr, expr1, {:num, n-1}}}, deriv(expr1, x)} end
+#f(x) = ln(x), d(f(x))/dx = 1/x
+def deriv({:ln, expr1}, x) do {:mul, {:div, {:num, 1}, expr1}, deriv(expr1, x)} end
 
-def pprint({:num, n}) do "#{n}" end
-def pprint({:var, v}) do "#{v}" end
-def pprint({:add, expr1, expr2}) do "(#{pprint(expr1)} + #{pprint(expr2)})" end
-def pprint({:mul, expr1, expr2}) do "(#{pprint(expr1)} * #{pprint(expr2)})" end
-#def pprint({:pow, expr1, expr2}) do "(#{pprint(expr1, expr2)} ^ #{pprint()})" end
-#def pprint(:ln, x) do "1/x" end
+
+#The following 4 functions simplifies expressions based on the operator used recursively
+def simplify({:add, expr1, expr2}) do simplify_add(simplify(expr1), simplify(expr2)) end
+def simplify({:mul, expr1, expr2}) do simplify_mul(simplify(expr1), simplify(expr2)) end
+def simplify({:powr, expr1, expr2}) do simplify_powr(simplify(expr1), simplify(expr2)) end
+def simplify({:div, expr1, expr2}) do simplify_div(simplify(expr1), simplify(expr2)) end
+def simplify(e) do e end
+
+def simplify_add(expr2, {:num, 0}) do expr2 end #x+0 = x
+def simplify_add({:num, 0}, expr1) do expr1 end #0+x = x
+def simplify_add({:num, expr1}, {:num, expr2}) do {:num, expr1 + expr2} end #1+1 = 2
+def simplify_add(expr1, expr2) do {:add, expr1, expr2} end #x+1 = x+1
+
+def simplify_mul({:num, 1}, expr1) do expr1 end #1*x = x
+def simplify_mul(expr2, {:num, 1}) do expr2 end #x*1 = x
+def simplify_mul({:num, 0}, _) do {:num, 0} end #x*0 = 0
+def simplify_mul(_, {:num, 0}) do {:num, 0} end #0*x = 0
+def simplify_mul({:num, expr1}, {:num, expr2}) do {:num, expr1*expr2} end #3*2 = 6
+def simplify_mul(expr1, expr2) do {:mul, expr1, expr2} end #x*e = x*e
+
+def simplify_powr(_, {:num, 0}) do {:num, 1} end #x^0 = 1
+def simplify_powr(expr1, {:num, 1}) do expr1 end #x^1 = x
+def simplify_powr(expr1, expr2) do {:powr, expr1, expr2} end #e^x = e^x
+
+def simplify_div(expr1, {:num, 0}) do "Error: Div by zero" end
+def simplify_div(expr1, {:num, 1}) do expr1 end
+def simplify_div(expr1, expr2) do {:div, expr1, expr2} end
+
+def pprint({:num, n}) do "#{n}" end #Prints a number
+def pprint({:var, v}) do "#{v}" end #Prints variable
+def pprint({:add, expr1, expr2}) do "#{pprint(expr1)} + #{pprint(expr2)}" end #Prints addition
+def pprint({:mul, expr1, expr2}) do "(#{pprint(expr1)} * #{pprint(expr2)})" end #Prints multiplication
+def pprint({:powr, expr1, expr2}) do "#{pprint(expr1)} ^(#{pprint(expr2)})" end #Prints exponents
+def pprint({:div, expr1, expr2}) do "(#{pprint(expr1)}) / (#{pprint(expr2)})" end
+def pprint({:ln, expr1}) do "ln#{pprint(expr1)}" end
 
 end
